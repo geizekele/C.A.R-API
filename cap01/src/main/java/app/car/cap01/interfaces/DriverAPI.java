d54c285d045606b2760bb9b2d2f9f188598f089a
Geize Kele <ge.kele@outlook.com>
2024-01-22 23:56:47 -0300

    Added list

--- cap01/src/main/java/app/car/cap01/interfaces/DriverAPI.java
+++ cap01/src/main/java/app/car/cap01/interfaces/DriverAPI.java
@@ -1,5 +1,13 @@
 package app.car.cap01.interfaces;
 
+import app.car.cap01.domain.Driver;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DriverAPI {
+	
+	public List<Driver> listDrivers(){
+		return new ArrayList<>();
+	}
 
 }
